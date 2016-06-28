e46d502324a4bd4e5515ba05960d5940258c3c2e
Austin Hudson <arhudson7@gmail.com>
2016-06-28 11:22:47 -0400

    created a basic form of the voice activation

--- gofish.js
+++ gofish.js
@@ -5,12 +5,16 @@ var SpeechRecognition = window.webkitSpeechRecognition ||   //prefixes for cross
                         window.msSpeechRecognition     ||
                         window.oSpeechRecognition      ||
                         window.SpeechRecognition,
-    recongition       = new SpeechRecognition(),
+    recognition       = new SpeechRecognition(),
     deck              = new Deck(),
+    development       = true,
     inProgress        = false;
 
-//start game button
+//get the start button and make an event listener for it
 var startButton = document.querySelector('button');
+startButton.addEventListener('click', function() {
+  start();
+});
 /*
 This function starts the game
 */
@@ -23,8 +27,51 @@ function start(){
   inProgress = true;
   //hide the start button
   startButton.classList.add("hidden");
-
+  //initalize the sound recognition
+  initializeRecognition();
+  //start the game
+  playGame();
 }
 
-console.log(deck);
-start();
+/*
+ This function sets up parameters for the speech recognition and starts it listening
+*/
+function initializeRecognition() {
+  recognition.continuous = true;                          // it's always listening
+  if (development) {recognition.interimResults = true;}   // logs interim results to the console in dev mode
+  recognition.onend = function() {                        // restarts the recognition engine if there's a pause in speech
+    if (inProgress) {
+      console.log("restarting recognition");
+      recognition.start();
+    }
+  };
+  recognition.start();                        // start listening
+}
+
+/*
+ This function will listen to the user's speech result
+*/
+function speechResult() {
+  recognition.onresult = function(event) {
+   var transcript = event.results[event.resultIndex][0].transcript;
+
+   var words = transcript.split(" ");
+   var result = words[words.length - 1];
+
+   if (development) {
+     var dev = document.querySelector('#speech');
+     dev.innerHTML = "";
+     dev.innerHTML = "Last speech recognition result: <span class='italics'>" + result + "</span>";
+   }
+   };
+ }
+/*
+ This function starts the game
+*/
+function playGame() {
+  if (inProgress) {
+    speechResult();
+  } else {
+    endGame();
+  }
+}
